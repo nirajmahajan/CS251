61983a4433f36e4469a671183ab2312677e54cc6
neelaryan2 <49657230+neelaryan2@users.noreply.github.com>
2019-08-04 16:53:06 +0530

    Update helpGroupTheCsv.sh

--- outLabs/outlab1/solution/Task3/helpGroupTheCsv.sh
+++ outLabs/outlab1/solution/Task3/helpGroupTheCsv.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-bew=$(awk -v FS='[,]' '{print $3,$1,$2"\n"$5,$1,$4"\n"$7,$1,$6}' OFS=, $1)
-dew=$(echo "$bew" | sort)
-cew=$(echo "$dew" | awk -v FS='[,]' '{print $2,$3,$1}' OFS=,)
-echo "$cew" | cat > $2
+> $2
+a1=$(cut -d ',' -f 1,2,3 $1)
+a2=$(cut -d ',' -f 1,4,5 $1)
+a3=$(cut -d ',' -f 1,6,7 $1)
+echo "$a1" | cat >> $2
+echo "$a2" | cat >> $2
+echo "$a3" | cat >> $2
+a5=$(sort -t',' -k3 $2)
+echo "$a5" | cat >$2
