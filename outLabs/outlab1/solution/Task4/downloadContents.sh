013b3189878b30505e084f67030074bab7f186b7
Niraj Mahajan <nirajmahajan007@gmail.com>
2019-08-04 07:23:01 +0530

    Task4 Part2 Done

---
+++ outLabs/outlab1/solution/Task4/downloadContents.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [ ! $# -eq 2 ]; then
+	echo "Usage:  ./downloadContents.sh <url> <directory_path>"
+	exit 1
+fi
+
+CURR_PATH=$(pwd)
+
+cd "$2"
+
+wget -qr -np --domains "$(echo "$url" | sed -e 's/^.*\/\///' -e 's/\/.*$//')" "$1"
+
+cd "$CURR_PATH"
+
+tree -J "$2" > "urlReport.json"
+
+md5sum "urlReport.json"
+
+COUNT=$(grep -c "{" "urlReport.json")
+echo "$COUNT"
+
+ANS=$(ps -q "$COUNT" -o comm=)
+
+if [ "$?" -eq 1 ]; then
+	echo "No such process"
+else
+	echo "$ANS"
+fi
