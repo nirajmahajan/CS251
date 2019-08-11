307820c60530f1c011f9a9e0b75078dcd205a60f
neelaryan2 <49657230+neelaryan2@users.noreply.github.com>
2019-08-11 14:37:20 +0530

    Update departmentFilter.sh

--- outLabs/outlab2/solution/Task2/E/departmentFilter.sh
+++ outLabs/outlab2/solution/Task2/E/departmentFilter.sh
@@ -3,12 +3,12 @@ chmod 755 $1/*
 a=$1/*.txt
 for var in $a
 do
-	out=$( awk 'BEGIN{FS=":"}NR==4{print $2}' $var | sed 's/ //g' )
-	mv -t ./$out $var >& /dev/null
+	out=$( awk 'BEGIN{FS=":"}NR==5{print $2}' $var | sed 's/ //g' )
+	cp -t ./$out $var >& /dev/null
 	if [ $? -eq 1 ]
 	then
 		mkdir -p ./$out
-		mv $var ./$out
+		cp $var ./$out
 	fi
 done
 chmod -R 755 *
