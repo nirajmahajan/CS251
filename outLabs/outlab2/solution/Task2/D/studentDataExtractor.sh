df8228abab5d766403957a334567b06b5df4e7c3
neelaryan2 <49657230+neelaryan2@users.noreply.github.com>
2019-08-11 14:24:08 +0530

    Update studentDataExtractor.sh

--- outLabs/outlab2/solution/Task2/D/studentDataExtractor.sh
+++ outLabs/outlab2/solution/Task2/D/studentDataExtractor.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-sed '/^ *$/d' $1 | 
-sed 's/^-*-$/\#/' | 
-tr -cd '\11\12\15\40-\176' | 
-sed '1d' | 
-awk 'BEGIN{RS="\n#\n";FS=":|\n"} NF>4 {print > $4".txt"}' 
+sed '/^$/d' $1 | 
+tr -dc '\0-\177' | 
+awk 'BEGIN{RS="-";FS=":|\n"} NF>4 {print > $5".txt"}' 
 chmod 755 *
+
+#sed '/^ *$/d' $1 | 
+#sed 's/^-*-$/\#/' | 
+#tr -dc '\0-\177' | 
+#sed '1d' | 
+#awk 'BEGIN{RS="\n#\n";FS=":|\n"} NF>4 {print > $4".txt"}' 
+#chmod 755 *
