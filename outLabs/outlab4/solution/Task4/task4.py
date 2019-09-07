d54926949315e8f9ce20eebe9298a4a8d20f913d
Neel Aryan <49657230+neelaryan2@users.noreply.github.com>
2019-09-08 02:20:10 +0530

    Update task4.py

--- outLabs/outlab4/solution/Task4/task4.py
+++ outLabs/outlab4/solution/Task4/task4.py
@@ -1,16 +1,15 @@
 import numpy as np
 
 def mean_filter(arr,kernel_size):
-	a=np.cumsum(arr,dtype=float)
 	k=int((kernel_size-1)/2)
-	bool_a=np.ones((a.size,),dtype=bool)
-	bool_a[:k]=False
+	bool_a=np.ones((arr.size,),dtype=bool)
+	a=np.append(np.zeros((k+1,)),np.cumsum(arr,dtype=float))
 	bool_a[arr.size-k:]=False
 	temp=np.arange(0,arr.size)
 	out=np.zeros((arr.size,),dtype=float)
-	out[bool_a]=a[temp[bool_a]+k]-a[temp[bool_a]-k-1]
-	out[np.logical_not(bool_a)]=0
-	out[k]=a[kernel_size-1]
+	out[bool_a]=a[temp[bool_a]+2*k+1]-a[temp[bool_a]]
+	bool_a=np.logical_not(bool_a)
+	out[bool_a]=a[arr.size+k]-a[temp[bool_a]]
 	return out/kernel_size
 
 def generate_sin_wave(period,range_,num):
