66a306aece4347f6aab12629703ab0a966cb974d
rishiA <rishiapril2000@gmail.com>
2019-10-04 14:24:01 +0530

    now i have checked it,it seems good

--- outLabs/outlab7/outlab7-Ctrl Alt Defeat/average_runs.py
+++ outLabs/outlab7/outlab7-Ctrl Alt Defeat/average_runs.py
@@ -3,17 +3,19 @@ mydb=sqlite3.connect('ipl.db')
 c=mydb.cursor()
 
 c.execute('''
-	SELECT venue_name,AVG(runs_sum)
+	SELECT venue_name,AVG(runs_sum) AS avg
 	FROM
 	(
-		SELECT MATCH.venue_name AS venue_name,SUM(runs_scored+extra_runs) AS runs_sum,
+		SELECT MATCH.venue_name AS venue_name,SUM(runs_scored+extra_runs) AS runs_sum
 		FROM BALL_BY_BALL
 		INNER JOIN MATCH ON MATCH.match_id=BALL_BY_BALL.match_id
+		GROUP BY BALL_BY_BALL.match_id
 	)
-	GROUP BY venue_name;
+	GROUP BY venue_name
+	ORDER BY avg DESC ;
 	'''
 	)
 rows=c.fetchall()
 
 for i in rows:
-	print(i)
+	print("{},{}".format(i[0],i[1]))
