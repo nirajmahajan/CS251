f0a4ffe9d0ed712d655b5d5ee8df709dbf417381
Neel Aryan <neelaryan2@gmail.com>
2019-10-05 21:51:44 +0530

    corrected outlab7 task5

--- outLabs/outlab7/outlab7-Ctrl Alt Defeat/average_balls.py
+++ outLabs/outlab7/outlab7-Ctrl Alt Defeat/average_balls.py
@@ -9,7 +9,7 @@ c.execute('''
 		SELECT PLAYER.player_id as id, PLAYER.player_name AS name,  COUNT(striker) AS ball_count
 		FROM BALL_BY_BALL
 		INNER JOIN PLAYER ON PLAYER.player_id = BALL_BY_BALL.striker
-		GROUP BY BALL_BY_BALL.match_id	
+		GROUP BY BALL_BY_BALL.striker, BALL_BY_BALL.match_id
 	)
 	GROUP BY id
 	ORDER BY avrg DESC ;
@@ -19,9 +19,9 @@ row = c.fetchall()
 mydb.close()
 
 for i in row[:10]:
-    print("{},{},{}".format(i[0], i[1], round(i[2])))
+    print("{},{},{}".format(i[0], i[1], i[2]))
 ref = row[9]
 j = 10
 while row[j][-1] == ref[-1]:
-    print("{},{},{}".format(row[j][0], row[j][1], round(row[j][2])))
+    print("{},{},{}".format(row[j][0], row[j][1], row[j][2]))
     j += 1
