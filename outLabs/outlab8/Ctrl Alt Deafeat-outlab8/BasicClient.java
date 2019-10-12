b6974927784dfc3e2103e837db98af0f705e005f
rishiA <53684457+rishiagarwal2000@users.noreply.github.com>
2019-10-12 19:10:24 +0530

    task 1, delta need to be changed acc to latest announcements

---
+++ outLabs/outlab8/Ctrl Alt Deafeat-outlab8/BasicClient.java
@@ -0,0 +1,105 @@
+import java.io.*;
+import java.net.*;
+import java.util.*; 
+//check closing the sockets
+public class BasicClient {
+    public static void main(String[] args) throws UnknownHostException, IOException, InterruptedException{
+         
+        if (args.length != 1) {
+            System.err.println(
+                "Usage: java EchoClient <host name> <port number>");
+            System.exit(1);
+        }
+        int delta=0;
+        Map<Integer,Integer> m= new HashMap<Integer,Integer>();
+        String filename = args[0];
+        File file=new File(filename);
+        //int portNumber = Integer.parseInt(args[1]);
+        BufferedReader stdIn =
+                new BufferedReader(
+                    new FileReader(file));
+        String userInput=stdIn.readLine();
+        while(userInput!=null){
+            String[] ar=userInput.split(" ",3);
+            if(ar[0].equals("connect")){ 
+                try (
+                    Socket echoSocket = new Socket(ar[1], Integer.parseInt(ar[2]));
+                    PrintWriter out =
+                        new PrintWriter(echoSocket.getOutputStream(), true);
+                    BufferedReader in =
+                        new BufferedReader(
+                            new InputStreamReader(echoSocket.getInputStream()));
+                    
+                ) {
+                    System.out.println("OK");
+                    //System.out.print("till");
+                    int serverInput;
+                    userInput=stdIn.readLine();
+                    //System.out.print("till");
+                    String[] myin=userInput.split(" ",2);
+                    //System.out.print("till");
+                    while (!myin[0].equals("disconnect")) {
+                        //System.out.print("till");
+                        if(myin[0].equals("read")){
+                                out.println(userInput);
+                                //System.out.print("till");
+                                serverInput=Integer.parseInt(in.readLine());
+                                int i=Integer.parseInt(myin[1]);
+                                int change;
+                                if(m.containsKey(i))
+                                {
+                                    change=serverInput-m.put(i,serverInput);
+                                    delta=delta+change;
+                                }
+                                else
+                                {
+                                    m.put(i,serverInput);
+                                    change=serverInput;
+                                    delta=delta+serverInput;
+                                }
+                                System.out.print(serverInput);
+                                System.out.print(" ");
+                                System.out.println(change);
+                        }
+                        else{
+                                //System.out.print("till");
+                                out.println(userInput);
+                                //System.out.print("till");
+                                serverInput=Integer.parseInt(in.readLine());
+                                //System.out.print("till");
+                                //System.out.println(serverInput);
+                        }
+                        userInput=stdIn.readLine();
+                        myin=userInput.split(" ",2);
+                    }
+                    out.println("disconnect");
+                    out.close();
+                    in.close();
+                    echoSocket.close();
+                    System.out.println("OK");
+                    //Thread.sleep(100);
+                    userInput=stdIn.readLine();
+                    m= new HashMap<Integer,Integer>();
+                } catch (UnknownHostException e) {
+                    System.out.println("No Server");
+                    System.exit(1);
+                } catch (IOException e) {
+                    System.err.println("Couldn't get I/O for the connection to RA");
+                    //System.out.println("No Server");
+                    System.exit(1);
+                }
+            }
+            else if(ar[0].equals("sleep"))
+            {
+                Thread.sleep(Integer.parseInt(ar[1]));
+                userInput=stdIn.readLine();
+            }
+            else
+            {
+                System.out.print("Wrong place");
+                System.exit(1);
+            }
+        }
+        System.out.println("Delta is : " + delta);
+    }
+}
---
+++ outLabs/outlab8/Ctrl Alt Deafeat-outlab8/BasicServer.java
@@ -0,0 +1,81 @@
+import java.net.*;
+import java.io.*;
+import java.util.*;
+//check closing the sockets
+public class BasicServer {
+    public static void main(String[] args) throws IOException {
+         
+        
+        System.out.println("Listening on 5000"); 
+        Map<Integer,Integer> m= new HashMap<Integer,Integer>();
+        while(true){ 
+            try (
+
+                ServerSocket serverSocket =
+                    new ServerSocket(5000);
+                Socket clientSocket = serverSocket.accept();     
+                PrintWriter out =
+                    new PrintWriter(clientSocket.getOutputStream(), true);                   
+                BufferedReader in = new BufferedReader(
+                    new InputStreamReader(clientSocket.getInputStream()));
+            ) 
+
+            {
+                
+                int portNumber = 5000;
+                String inputLine;
+                inputLine = in.readLine();
+                String[] arr=inputLine.split(" ",2);
+                while (!arr[0].equals("disconnect")) {
+                    
+                    //System.out.println(arr[0]+"@"+arr[1]);
+                    if(arr[0].equals("add"))
+                    {
+                        //System.out.println("HERE");
+                        int i=Integer.parseInt(arr[1]);
+                        int ans;
+                        if(m.containsKey(i))
+                        {
+                            ans=m.put(i,m.get(i)+1);
+                            ans++;
+                        }
+                        else
+                        {
+                            m.put(i,1);
+                            ans=1;
+                        }
+                        out.println(ans);
+                        System.out.println("ADD "+i);
+                    }
+                    else if(arr[0].equals("read"))
+                    {
+                        int i=Integer.parseInt(arr[1]);
+                        int ans;
+                        if(m.containsKey(i))
+                        {
+                            ans=m.get(i);
+                        }
+                        else
+                        {
+                            ans=0;
+                        }
+                        out.println(ans);
+                        System.out.println("READ "+i+" "+ans);   
+                    }
+                    inputLine = in.readLine();
+                    arr=inputLine.split(" ",2); 
+                }
+        
+                System.out.println("DIS");
+                m= new HashMap<Integer,Integer>();
+                out.close();in.close();
+                serverSocket.close();
+            } catch (IOException e) {
+                System.out.println("here");
+                /*System.out.println("Exception caught when trying to listen on port "
+                    + portNumber + " or listening for a connection");
+                System.out.println(e.getMessage());*/
+            }
+        }
+    }
+}
