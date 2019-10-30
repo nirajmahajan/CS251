667e0b0aae4aa45c5d0c1a8bb7cb90ca7fc27147
Niraj Mahajan <nirajmahajan007@gmail.com>
2019-10-31 01:57:39 +0530

    Added about activity

--- outLabs/outlab9/Ctrl Alt Defeat-outlab9/Planner69/app/src/main/java/android/example/Planner/About.java
+++ outLabs/outlab9/Ctrl Alt Defeat-outlab9/Planner69/app/src/main/java/android/example/Planner/About.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class About extends GameTemplate {
+public class About extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
