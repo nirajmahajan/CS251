0cbbdfd38c0c8b5610bdbddf83c0e66cbf17fc76
Neel Aryan <neelaryan2@gmail.com>
2019-11-02 23:10:17 +0530

    almost completed outlab10

---
+++ outLabs/outlab10/outlab10-Ctrl Alt Defeat/q3/LinkList/LinkList.java
@@ -0,0 +1,144 @@
+ 
+public class LinkList<T> {
+ 
+    private Node<T> front;
+    private Node<T> rear;
+     
+    /*!
+    * \param item The element to be inserted at the front of the LinkList
+    * Inserts the element to the front of the LinkList,
+    * links the old front node to the new one
+    * and updates the front node of LinkList
+    * Prints the element being added to the front
+    */
+    public void insertFront(T item){
+        //! add element at the beginning of the queue
+        System.out.println("adding at front: "+item);
+        Node<T> nd = new Node<T>();
+        nd.setValue(item);
+        nd.setNext(front);
+        if(front != null) front.setPrev(nd);
+        if(front == null) rear = nd;
+        front = nd;
+    }
+     
+    /*!
+    * \param item The element to be inserted at the rear of the LinkList
+    * Inserts the element to the rear of the LinkList,
+    * links the old rear node to the new one
+    * and updates the rear node of LinkList
+    * Prints the element being added to the rear
+    */
+    public void insertRear(T item){
+        //! add element at the end of the queue
+        System.out.println("adding at rear: "+item);
+        Node<T> nd = new Node<T>();
+        nd.setValue(item);
+        nd.setPrev(rear);
+        if(rear != null) rear.setNext(nd);
+        if(rear == null) front = nd;
+         
+        rear = nd;
+    }
+     
+    /*!
+    * Removes the element at the front of the LinkList,
+    * links the new front node to null
+    * and updates the front node of LinkList
+    * to the second node in the old List
+    * Prints the element being removed from the front
+    */
+    public void removeFront(){
+        if(front == null){
+            System.out.println("Deque underflow!! unable to remove.");
+            return;
+        }
+        //! remove an item from the beginning of the queue
+        Node<T> tmpFront = front.getNext();
+        if(tmpFront != null) tmpFront.setPrev(null);
+        if(tmpFront == null) rear = null;
+        System.out.println("removed from front: "+front.getValue());
+        front = tmpFront;
+    }
+     
+    /*!
+    * Removes the element at the rear of the LinkList,
+    * links the new rear node to null
+    * and updates the rear node of LinkList
+    * to the second last node in the old List
+    * Prints the element being removed from the rear
+    */
+    public void removeRear(){
+        if(rear == null){
+            System.out.println("Deque underflow!! unable to remove.");
+            return;
+        }
+        //! remove an item from the beginning of the queue
+        Node<T> tmpRear = rear.getPrev();
+        if(tmpRear != null) tmpRear.setNext(null);
+        if(tmpRear == null) front = null;
+        System.out.println("removed from rear: "+rear.getValue());
+        rear = tmpRear;
+    }
+     
+    /*!
+    *  The main driver code for building the LinkList
+    */
+    public static void main(String a[]){
+        LinkList<Integer> deque = new LinkList<Integer>();
+        deque.insertFront(34);
+        deque.insertFront(67);
+        deque.insertFront(29);
+        deque.insertFront(765);
+        deque.removeFront();
+        deque.removeFront();
+        deque.removeFront();
+        deque.insertRear(43);
+        deque.insertRear(83);
+        deque.insertRear(84);
+        deque.insertRear(546);
+        deque.insertRear(356);
+        deque.removeRear();
+        deque.removeRear();
+        deque.removeRear();
+        deque.removeRear();
+        deque.removeFront();
+        deque.removeFront();
+        deque.removeFront();
+    }
+}
+
+/*!
+ * The Node class.
+ * This class defines the node structure
+ * which consists of a value, the next node
+ * and the previous node in reference to itself
+ * Each node defines each element of LinkList
+ * All the variables are private, so public functions 
+ * are made to fetch or set those variables
+*/ 
+class Node<T>{
+     
+    private Node<T> prev;
+    private Node<T> next;
+    private T value;
+     
+    public Node<T> getPrev() {
+        return prev;
+    }
+    public void setPrev(Node<T> prev) {
+        this.prev = prev;
+    }
+    public Node<T> getNext() {
+        return next;
+    }
+    public void setNext(Node<T> next) {
+        this.next = next;
+    }
+    public T getValue() {
+        return value;
+    }
+    public void setValue(T value) {
+        this.value = value;
+    }
+}
---
+++ outLabs/outlab10/outlab10-Ctrl Alt Defeat/q3/array/array.cpp
@@ -0,0 +1,245 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+/** \param myArr the array to be filled
+  * \return Always returns 0 (useless to the code) after filling
+  * 
+  * This function is used to fill a given
+  * 7x7 matrix with random numbers 
+  * between 20 and 419 (both inclusive)
+*/
+int fillArray(int myArr[7][7])                 
+{
+	for(int i=0;i<7;i++)
+	{
+		for(int j=0;j<7;j++)                    
+		{
+			myArr[i][j]=20+rand()%400;              
+		}
+	}
+	return 0;
+}
+
+/** \param myArr the array whose spiral path will be printed
+  * 
+  * This function prints the straight segments
+  * of the path in a 2D array which starts 
+  * from the top left corner (0,0), spiralling 
+  * into the center of the matrix
+*/
+void printSpiral(int myArr[7][7])
+{
+	cout << "Array Spiral Function." << endl;
+	for(int j=0;j<1;j++)                           
+    {                                              
+        for(int i=0;i<7;i++)
+        {
+            cout<< myArr[i][j]<< "  ";
+        }
+
+    }
+    cout << endl;
+
+    for(int i=6;i<7;i++)                          
+    {                                            
+        for(int j=1;j<7;j++)
+        {
+            cout << myArr[i][j] << "  ";
+        }
+    }
+    cout << endl;
+
+    for(int j=6;j<7;j++)                          
+    {                                             
+        for(int i=5;i>=0;i--)
+        {
+            cout << myArr[i][j] << "  ";
+        }
+    }
+    cout << endl;
+
+    for (int i=0;i<1;i++)                          
+    {                                              
+        for (int j=5;j>=1;j--)
+        {
+            cout << myArr[i][j] << "  ";
+        }
+    }
+    cout << endl;
+
+    for (int j=1; j<2;j++)
+    {                                             
+        for (int i=1; i<6;i++)
+        {
+            cout << myArr[i][j] << "  ";
+        }
+    }
+    cout << endl;
+
+    for (int i=5; i<6;i++)                        
+    {
+        for (int j=2; j<6; j++)
+        {
+            cout << myArr[i][j] << "  ";
+        }
+    }
+    cout << endl;
+
+    for (int j=5; j<6; j++)                        
+    {
+        for (int i=4; i>0; i--)
+        {
+            cout << myArr[i][j] << "  ";
+        }
+    }
+    cout << endl;
+
+    for (int i=1; i<2; i++)                        
+    {
+       for (int j=4; j>1; j--)
+       {
+           cout << myArr[i][j] << "  ";
+       }
+    }
+    cout << endl;
+
+    for (int j=2; j<3; j++)                        
+    {
+        for (int i=2; i<5; i++)
+        {
+            cout << myArr[i][j] << "  ";
+        }
+    }
+    cout << endl;
+
+    for (int i=4; i<5; i++)                        
+    {
+        for (int j=3; j<5; j++)
+        {
+            cout << myArr[i][j] << "  ";
+        }
+    }
+    cout << endl;
+
+    for (int j=4; j<5; j++)                        
+    {
+        for (int i=3; i>1; i--)
+        {
+            cout << myArr[i][j] << "  ";
+        }
+    }
+    cout << endl;
+
+    for (int i=2; i<3; i++)                        
+    {
+        for (int j=3; j<4; j++)
+        {
+            cout << myArr[i][j] << "  ";
+        }
+    }
+    cout << endl;
+
+    for (int i=3; i<4; i++)                         
+    {
+        for (int j=3; j<4; j++)
+        {
+            cout << myArr[i][j] << "  ";
+        }
+    }
+  cout << endl << endl;
+}
+
+void printCol(int myArr[7][7], int s, int e)                     
+{  
+	cout << "Column Output" << endl;                
+	for (int j=s-1; j<e-1; j++)
+	{                                               
+		for (int i=0; i<7; i++)
+		{
+			cout << myArr[i][j] << " ";
+		}
+		cout << endl;
+	}
+	cout << endl << endl;
+}
+
+/** \param myArr the array to be analysed
+  * \return the minimum value present in the 2D array
+  * 
+  * This function computes the smallest value of all
+  * the elements in the 2D array
+*/
+int findMin (int myArr[7][7])
+{
+	int i,j;
+	int min = myArr[0][0];
+
+	for (i=0; i<7; i++)                             
+	{
+		for (j=0; j<7; j++)
+		{
+			if(myArr[i][j] < min)                   
+			{                                       
+				min = myArr[i][j];
+			}                                       
+		}                                           
+
+	}
+    return min;
+}
+
+/** \param myArr the array to be analysed
+  * \return Average value of array elements
+  * 
+  * This function returns the average of all
+  * the elements of the 2D array, truncated to 
+  * the nearest lower integer (or floored value)
+*/
+int findAverage (int myArr[7][7])                  
+{                                                   
+	int i,j;
+	int sum = 0;
+
+	for (i=0; i<7; i++)                             
+	{
+	    for (j=0; j<7; j++)
+	    {
+	        sum = sum + myArr[i][j];
+	    }
+
+	}
+    return sum/49;       
+}
+
+void printArray (int myArr[7][7])                     
+{
+    cout << "Normal Array Output" << endl;
+
+    for (int i =0; i<7; i++)
+    {
+        for (int j=0; j<7; j++)
+        {
+            cout << myArr[i][j] << "  ";
+        }
+        cout << endl;
+    }
+    cout << endl;                                   
+    cout << endl;
+}
+
+int main(){
+	
+	int myArr [7][7];
+	fillArray(myArr);                               
+	printArray(myArr);
+	printSpiral(myArr);
+	printCol(myArr, 2, 5);
+	cout<<"Min:"<<findMin(myArr);
+	cout<<" Average:"<<findAverage(myArr);
+    cout<<endl;
+
+	return 0;
+}
+ 
